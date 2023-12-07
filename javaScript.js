3fd8385b50fefb0349faae9d346f736e28fab753
abdu61 <112977047+abdu61@users.noreply.github.com>
2023-12-07 20:59:21 +0300

    Added intial solution

--- javaScript.js
+++ javaScript.js
@@ -31,10 +31,56 @@ function operate(operator, a, b){
 }
 
 let display = document.querySelector("#show");
-let buttons = document.querySelectorAll(".button");
+let buttons = document.querySelectorAll(".buttonNo");
 let clear = document.querySelector("#clear");
 let equals = document.querySelector("#equals");
 let decimal = document.querySelector("#decimal");
 let backspace = document.querySelector("#backspace");
+let operators = document.querySelectorAll(".operator");
 let displayValue = "";
 let operator = "";
+
+buttons.forEach((button => {
+    button.addEventListener("click", () => {
+        displayValue += button.textContent;
+        display.textContent = displayValue;
+    })
+}
+))
+
+clear.addEventListener("click", () => {
+    displayValue = "";
+    display.textContent = displayValue;
+})
+
+backspace.addEventListener("click", () => {
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = displayValue;
+})
+
+decimal.addEventListener("click", () => {
+    if(!displayValue.includes(".")){
+        displayValue += ".";
+        display.textContent = displayValue;
+    }
+})
+
+operators.forEach((operatorButton => {
+    operatorButton.addEventListener("click", () => {
+        operator = operatorButton.textContent;
+        displayValue += operator;
+        display.textContent = displayValue;
+    })
+}))
+
+equals.addEventListener("click", () => {
+    let values = displayValue.split(operator);
+    let a = parseFloat(values[0]);
+    let b = parseFloat(values[1]);
+    displayValue = operate(operator, a, b);
+    display.textContent = displayValue;
+})
+
+
+
+
